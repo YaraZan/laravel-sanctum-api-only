399672a887dfad17c629335f8d8070afbfa3c308
andrei_potiomkin <89867134953@mail.ru>
2024-11-20 13:29:23 +0300

    feat(models): (#1) add role model, update user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,14 +3,16 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+
+use App\Traits\HasApiTokensWithLocation;
+use App\Traits\HasUuid;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
-    /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable, HasApiTokensWithLocation, HasUuid;
 
     /**
      * The attributes that are mass assignable.
@@ -29,6 +31,8 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $hidden = [
+        'id',
+        'role_id',
         'password',
         'remember_token',
     ];
@@ -44,5 +48,15 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    public function isRoot(): bool
+    {
+        return optional($this->role)->name === 'root';
+    }
+
+    public function role()
+    {
+        return $this->belongsTo(Role::class);
     }
 }
